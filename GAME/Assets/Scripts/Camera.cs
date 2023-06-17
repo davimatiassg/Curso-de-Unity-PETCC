49177754fb0134e217e1c024e945e9ee5aa7ea38
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2023-06-17 15:25:55 -0300

    camera now follows player

---
+++ GAME/Assets/Scripts/Camera.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public float velocidade = 0.01f;
+    [SerializeField] private Transform jogador;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 novaPos = new Vector3(jogador.position.x, jogador.position.y + 1f, transform.position.z);
+        transform.position = Vector3.Slerp(transform.position, novaPos, velocidade);
+    }
+}
