0fbfa31250ed5c112b122158953d85ee7af5df64
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2023-06-17 15:28:22 -0300

    improved camera

--- GAME/Assets/Scripts/Camera.cs
+++ GAME/Assets/Scripts/Camera.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
-    public float velocidade = 0.01f;
+    public float velocidade = 2f;
     [SerializeField] private Transform jogador;
 
     // Update is called once per frame
     void Update()
     {
         Vector3 novaPos = new Vector3(jogador.position.x, jogador.position.y + 1f, transform.position.z);
-        transform.position = Vector3.Slerp(transform.position, novaPos, velocidade);
+        transform.position = Vector3.Slerp(transform.position, novaPos, velocidade*Time.deltaTime); 
     }
 }
