2bd743fd33c7946af258cc87c56763cd19597b31
davimatiassg <davimatiassg@gmail.com>
2023-07-26 08:14:03 -0300

    title screen

--- GAME/Assets/Scripts/Menu.cs
+++ GAME/Assets/Scripts/Menu.cs
@@ -9,28 +9,32 @@ public class Menu : MonoBehaviour
     [SerializeField] GameObject optionsMenu;
 
     private bool menuOpcoesAberto = false;
+    UnityEngine.UI.Slider volumeMusicaSlider, volumeEfeitosSlider;
 
     void Start()
     {
         // Carregar as configurações de volume atuais
-        Slider volumeMusicaSlider = optionsMenu.transform.GetChild(0).gameObject.GetComponent<Slider>();
-        Slider volumeEfeitosSlider = optionsMenu.transform.GetChild(1).gameObject.GetComponent<Slider>();
+        volumeMusicaSlider = optionsMenu.transform.GetChild(0).gameObject.GetComponent<Slider>();
+        volumeEfeitosSlider = optionsMenu.transform.GetChild(1).gameObject.GetComponent<Slider>();
 
         volumeMusicaSlider.value = PlayerPrefs.GetFloat("VolumeMusica", 1f);
         volumeEfeitosSlider.value = PlayerPrefs.GetFloat("VolumeEfeitos", 1f);
-    }
-
-    public void Play() {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Jogo");
     }
 
     public void OptionsMenu() {
         menuOpcoesAberto = !menuOpcoesAberto;
         optionsMenu.SetActive(menuOpcoesAberto);
     }
+    public void SetVolumeMusica(){
+        PlayerPrefs.SetFloat("VolumeMusica", volumeMusicaSlider.value);
+        PlayerPrefs.Save();
+    }
+    public void SetVolumeEfeitos(){
+        PlayerPrefs.SetFloat("VolumeEfeitos", volumeEfeitosSlider.value);
+        PlayerPrefs.Save();
+    }
 
     public void Exit() {
-        Debug.Log("NUNNCAAAAAAAAAA !!!");
         Application.Quit();
     }
 
