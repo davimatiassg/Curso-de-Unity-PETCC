3d60f1ce70876ae205244f42e1789bf02538bcfb
davimatiassg <davimatiassg@gmail.com>
2023-07-26 06:54:42 -0300

    Title Splash

---
+++ GAME/Assets/Scripts/EndGameScore.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class EndGameScore : MonoBehaviour
+{
+    
+    [SerializeField] TMPro.TextMeshProUGUI scoreLabel;
+    
+    void Start(){
+        // Atualiza a UI com a quantidade de score
+        int scoreCurrent = PlayerPrefs.GetInt("score", 0);
+        scoreLabel.text = scoreCurrent.ToString();  
+    }
+}
