abc245b577f5376f1e05383d1dc716bb3b43192b
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2023-07-27 17:53:53 -0300

    edited comments in Scorer.cs

--- GAME/Assets/Scripts/Scorer.cs
+++ GAME/Assets/Scripts/Scorer.cs
@@ -6,19 +6,19 @@ using UnityEngine.UIElements;
 public class Scorer : MonoBehaviour
 {
     
-    [SerializeField] TMPro.TextMeshProUGUI scoreLabel;
+    [SerializeField] TMPro.TextMeshProUGUI scoreLabel; //!< Elemento da UI que mostra o score
 
-    // Quando o objeto colidir com outro objeto que tenha o componente Collider2D e que tenha a tag "coin" 
+    /// Quando o objeto colidir com outro objeto que tenha o componente Collider2D e que tenha a tag "coin" 
 
     public void UpdateScore(){
-        // Atualiza a UI com a quantidade de score
+        /// Atualiza a UI com a quantidade de score
         int scoreCurrent = PlayerPrefs.GetInt("score", 0);
         scoreLabel.text = scoreCurrent.ToString();  
     }
 
     public void AddScore(int bonus)
     {
-        int score = PlayerPrefs.GetInt("score", 0); // 0 é o valor padrão caso não exista a chave "score"
+        int score = PlayerPrefs.GetInt("score", 0); /// 0 é o valor padrão caso não exista a chave "score"
         score += bonus;
         PlayerPrefs.SetInt("score", score);
         PlayerPrefs.Save();
@@ -26,10 +26,10 @@ public class Scorer : MonoBehaviour
     }
 
     public int UpdateLives(int hp){
-        // Atualiza a UI com a quantidade de lifes
+        /// Atualiza a UI com a quantidade de lifes
         for (int i = 0; i < transform.childCount; i++)
         {
-            // Se o valor de i for menor que a quantidade de lifes Current, ativa a imagem da vida
+            /// Se o valor de i for menor que a quantidade de lifes Current, ativa a imagem da vida
             transform.GetChild(i).gameObject.SetActive(i < hp);
         }
 
