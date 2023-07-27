76503fe34ac9fb43542da7756d59da0dd4b1232b
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2023-07-27 17:46:27 -0300

    comments on Menu.cs

--- GAME/Assets/Scripts/Menu.cs
+++ GAME/Assets/Scripts/Menu.cs
@@ -11,9 +11,10 @@ public class Menu : MonoBehaviour
     private bool menuOpcoesAberto = false;
     UnityEngine.UI.Slider volumeMusicaSlider, volumeEfeitosSlider;
 
+    /// Start é chamado antes do primeiro update de frame
     void Start()
     {
-        // Carregar as configurações de volume atuais
+        /// Carregar as configurações de volume atuais
         volumeMusicaSlider = optionsMenu.transform.GetChild(0).gameObject.GetComponent<Slider>();
         volumeEfeitosSlider = optionsMenu.transform.GetChild(1).gameObject.GetComponent<Slider>();
 
@@ -21,19 +22,25 @@ public class Menu : MonoBehaviour
         volumeEfeitosSlider.value = PlayerPrefs.GetFloat("VolumeEfeitos", 1f);
     }
 
+    /// Ativa o menu
     public void OptionsMenu() {
         menuOpcoesAberto = !menuOpcoesAberto;
         optionsMenu.SetActive(menuOpcoesAberto);
     }
+
+    /// Modifica o volume da música de acordo com o slider do menu
     public void SetVolumeMusica(){
         PlayerPrefs.SetFloat("VolumeMusica", volumeMusicaSlider.value);
         PlayerPrefs.Save();
     }
+
+    /// Modifica o volume dos efeitos de acordo com o slider do menu
     public void SetVolumeEfeitos(){
         PlayerPrefs.SetFloat("VolumeEfeitos", volumeEfeitosSlider.value);
         PlayerPrefs.Save();
     }
 
+    /// Fecha o jogo
     public void Exit() {
         Application.Quit();
     }
