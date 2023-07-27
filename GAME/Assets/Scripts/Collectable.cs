f4c49c02c1f619f3d902fc38b4c3fdf9c3f2c345
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2023-07-27 16:56:21 -0300

    comments on Collectable.cs

--- GAME/Assets/Scripts/Collectable.cs
+++ GAME/Assets/Scripts/Collectable.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class Collectable : MonoBehaviour
 {
     public int scoreBonus = 50;
+
     void OnTriggerEnter2D(Collider2D collider)
     {
+        /// Se está colidindo com o player
         if (collider.gameObject.CompareTag("Player"))
         {
+            /// Adicionando os pontos
             collider.gameObject.GetComponent<Player>().Collect(scoreBonus);
+
             Destroy(this.gameObject);
         }
     }
