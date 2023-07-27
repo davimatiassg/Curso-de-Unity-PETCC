7903726424bed499ee9783d1d2e0085a11c93575
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2023-07-27 17:54:45 -0300

    edited comments in sfxScript.cs

--- GAME/Assets/Scripts/sfxScript.cs
+++ GAME/Assets/Scripts/sfxScript.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class sfxScript : MonoBehaviour
 {
-    // Lista de sons que estão sendo tocados
+    /// Lista de sons que estão sendo tocados
     public List<audioTuple> audioQueue = new List<audioTuple>();
 
-    // Tupla com o tempo de duração de um som e o som
+    /// Tupla com o tempo de duração de um som e o som
     public struct audioTuple
     {
         public float time;
@@ -27,12 +27,12 @@ public class sfxScript : MonoBehaviour
         for (int i = 0;i < audioQueue.Count;i ++)
         {
             if (audioQueue[i].clip == clip)
-            { // O som já está sendo tocado
+            { /// O som já está sendo tocado
                 newClip = false;
             }
 
             if (audioQueue[i].time <= 0)
-            { // O tempo restante do som acabou -> removendo o som da lista
+            { /// O tempo restante do som acabou -> removendo o som da lista
                 audioQueue.RemoveAt(i);
                 i --;
             }
@@ -42,7 +42,7 @@ public class sfxScript : MonoBehaviour
         }
 
         if (newClip)
-        { // Adicionando um som novo a lista
+        { /// Adicionando um som novo a lista
             audioQueue.Add(new audioTuple(clip.length + extratime, clip));
             GetComponent<AudioSource>().PlayOneShot(clip, volume);
         }
