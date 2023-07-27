686432541c90df2568dd2acef2e6a42f7e9455f8
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2023-07-27 16:48:43 -0300

    comments on Enemy.cs

--- GAME/Assets/Scripts/Enemy.cs
+++ GAME/Assets/Scripts/Enemy.cs
@@ -6,36 +6,45 @@ public class Enemy : MonoBehaviour, I_HitableObj
 {
     public GameObject Player;
 
+    /// Atributos
     [SerializeField] private int hp = 1;
-    [SerializeField] private GameObject hitVFX;
-    [SerializeField] private GameObject deathVFX;
-
     [SerializeField] private float speed = 3f;
     [SerializeField] private float jump = 25f;
     [SerializeField] private int atkDmg = 1;
     [SerializeField] private float scale = 1f;
 
-    private float disMax = 8.5f;
-    private float disMin = 0.5f;
-    private float disJump = 1.6f;
+    /// Movimentação
+    private float disMax = 8.5f; //!< Distância máxima para perseguir o jogador
+    private float disMin = 0.5f; //!< Distância mínima para perseguir o jogador
+    private float disJump = 1.6f; //!< Distância mínima vertical do jogador para pular
 
-    static private float jumpCd = 1.5f;
-    private float tJump = 0;
-    
-    static private float atkCd = 0.25f;
-    private float tAtk = 0;
+    static private float jumpCd = 1.5f; //!< Tempo mínimo entre pulos
+    private float tJump = 0; //!< Cooldown do jumpCd
 
     [SerializeField] private Transform floorChk;
     [SerializeField] private LayerMask solid;
-
+    
+    /// Ataque
+    static private float atkCd = 0.25f; //!< Tempo mínimo entre ataques
+    private float tAtk = 0; //!< Cooldown do atkCd
     private Rigidbody2D rb;
-    private Animator anim;
-    private AudioSource aud;
 
+    /// Animação
+    private Animator anim;
+
+    /// Sons
+    private AudioSource aud;
     [SerializeField] AudioClip stepSound;
     [SerializeField] AudioClip hurtSound;
     [SerializeField] AudioClip dyingSound;
 
+    /// Vfx
+    [SerializeField] private GameObject hitVFX;
+    [SerializeField] private GameObject deathVFX;
+
+    /// ------- Métodos da Unity -------
+
+    /// Start é chamado antes do primeiro update de frame
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -45,10 +54,13 @@ public class Enemy : MonoBehaviour, I_HitableObj
         scale = transform.localScale.x;
     }
 
+    /// Update é chamado uma vez por frame
     void Update()
     {
+        /// Direção para/Distância até  o player
         Vector2 pDir = Player.transform.position - transform.position;
 
+        /// Se o player está longe o bastante e perto o bastante para se perseguido
         if (Mathf.Abs(pDir.x) > disMin && Mathf.Abs(pDir.x) < disMax)
         {
             /// Persegue o Player
@@ -56,14 +68,16 @@ public class Enemy : MonoBehaviour, I_HitableObj
             anim.SetBool("Chase", true);
             transform.localScale = new Vector2(-Mathf.Sign(rb.velocity.x) * scale, scale);
 
-            /// Jump
+            /// Pulo
             if (tJump > 0){ tJump -= Time.deltaTime;}
             if (IsOnGround())
             {
                 RaycastHit2D ray = Physics2D.Raycast(transform.position, pDir, 1.25f);
                 bool obstacleAhead = ray.collider != null;
 
-                if ((Mathf.Abs(pDir.y) > disJump || obstacleAhead) && tJump <= 0)
+                /// Se o cooldown de pular já acabou e
+                /// (tem um obstaculo a frente ou a distância vertical até o player é o suficiente para pular)
+                if (tJump <= 0 && (obstacleAhead || Mathf.Abs(pDir.y) > disJump))
                 {
                     rb.velocity = new Vector2(rb.velocity.x, jump);
                     tJump = jumpCd;
@@ -78,11 +92,15 @@ public class Enemy : MonoBehaviour, I_HitableObj
         }
     }
 
+    /// ------- Métodos de controle das ações -------
+
+    /// Se está no chão
     private bool IsOnGround()
     {
         return Physics2D.OverlapCircle(floorChk.position, 0.01f, solid);
     }
 
+    /// Se está colidindo
     public void OnCollisionStay2D(Collision2D col)
     {
         if(col.gameObject.tag == "Player")
@@ -91,6 +109,7 @@ public class Enemy : MonoBehaviour, I_HitableObj
         }
     }
 
+    /// Se não está mais colidindo
     public void OnCollisionExit2D(Collision2D col)
     {
         if(col.gameObject.tag == "Player")
@@ -110,7 +129,7 @@ public class Enemy : MonoBehaviour, I_HitableObj
         }   
     }
 
-
+    /// Leva dano
     public void TakeHit(int damage, Vector2 hitPos)
     {
         if (hp > 0)
@@ -122,10 +141,13 @@ public class Enemy : MonoBehaviour, I_HitableObj
         if(hp <= 0)
         {
             Instantiate(deathVFX, transform.position, transform.rotation);
-            //Play death animation
+
+            /// Toca a animação de morte
             Destroy(this.gameObject, 0.10f);
         }
     }
+
+    /// Efeitos sonoros
 
     public void PlayDeathSound()
     {
