46e8cd0fbd1aacb57815e913635bd1e1fffe473b
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2023-07-27 17:07:12 -0300

    comment on EndGameScore.cs

--- GAME/Assets/Scripts/EndGameScore.cs
+++ GAME/Assets/Scripts/EndGameScore.cs
@@ -6,10 +6,10 @@ using UnityEngine.UIElements;
 public class EndGameScore : MonoBehaviour
 {
     
-    [SerializeField] TMPro.TextMeshProUGUI scoreLabel;
+    [SerializeField] TMPro.TextMeshProUGUI scoreLabel; //!< Elemento da UI que mostra o score
     
     void Start(){
-        // Atualiza a UI com a quantidade de score
+        /// Atualiza a UI com a quantidade de score
         int scoreCurrent = PlayerPrefs.GetInt("score", 0);
         scoreLabel.text = scoreCurrent.ToString();  
     }
