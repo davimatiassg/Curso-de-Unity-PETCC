c56fed2d6d02cf5aeac82d969d140093b804bc53
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2023-07-27 17:06:02 -0300

    comments on Boulder.cs

--- GAME/Assets/Scripts/Boulder.cs
+++ GAME/Assets/Scripts/Boulder.cs
@@ -11,8 +11,10 @@ public class Boulder : MonoBehaviour
 
     [SerializeField] AudioClip roll;
 
+    /// Update é chamado uma vez por frame
     public void Update()
     {
+        /// Se a pedra tem uma velocidade perceptível
         if (rb.velocity.magnitude > 0.001)
         {
             GetComponent<sfxScript>().playSoundContinuously(roll, 1f, 0.001f/rb.velocity.magnitude);
@@ -23,12 +25,14 @@ public class Boulder : MonoBehaviour
     {
         float relVel = col.relativeVelocity.magnitude;
         
+        /// Se a velocidade relativo entre o que está sendo colidido e a pedra for grande o bastante
+        /// e se a velocidade da pedra for grande o bastante
         if(relVel >= 4f && rb.velocity.magnitude >= 4f)
         {
             I_HitableObj hit = col.gameObject.GetComponent<I_HitableObj>();
             if(hit != null) 
             { 
-                Debug.Log(Mathf.InverseLerp(4f, 20f, relVel));
+                /// Aplicando dano no objeto colidido
                 hit.TakeHit(Mathf.CeilToInt(maxDmg * Mathf.InverseLerp(4f, 20f, relVel)), col.GetContact(0).point); 
             }
         }
