e7d89bf1747e6401ffd89aef9ef7d22fe8fc8806
CinquilCinquil <106356391+CinquilCinquil@users.noreply.github.com>
2023-07-27 16:59:19 -0300

    comments on DoorBehaviour.cs

--- GAME/Assets/Scripts/DoorBehaviour.cs
+++ GAME/Assets/Scripts/DoorBehaviour.cs
@@ -9,9 +9,13 @@ public class DoorBehaviour : MonoBehaviour
 
     public void OnTriggerEnter2D(Collider2D col)
     {
+        /// Se está colidindo com o player
         if(col.gameObject.tag == "Player")
         {
+            /// Ativa a animação "Open"
             anim.SetTrigger("Open");
+
+            /// Carregando um novo nível
             Levels.LoadNextLevel(0.5f);
         }
     }
